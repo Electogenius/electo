785755dfa555291e07de7b1a41f79dd9eb5230ae
Electogenius <69585636+Electogenius@users.noreply.github.com>
2021-05-05 18:42:03 +0530

    some changes

--- root/script.js
+++ root/script.js
@@ -1,21 +1,27 @@
 function n(x) {
 	return (typeof x == "string") ? x : x.join(" ")
 }
+
 function syncStore() {
 	var e = "store"
-	if(t.dir!="/"){
-	t.dir.split('/').slice(1).forEach(f=>{
-		e+="['"+f+"']"
-	})}
+	if (t.dir != "/") {
+		t.dir.split('/').slice(1).forEach(f => {
+			e += "['" + f + "']"
+		})
+	}
 	//console.log(e);
-	eval(e+"=t.dirObj")
+	try {
+		eval(e + "=t.dirObj")
+	} catch (e) {
+		console.log("error changing directory");
+	}
 	localStorage.setItem("t", JSON.stringify(store))
 }
 
 var store = {}
 var def = {
 	"home": {
-		"welcome.egc": "log(Welcome to my odd website. You are smart enough to discover this)"
+		"welcome.egc": "log(Welcome to my odd website. You are smart enough to discover this.)"
 	},
 	root: {
 
@@ -33,20 +39,31 @@ var t = {
 	},
 	dirObj: store,
 	error: c => t.t.error(c),
+	help: {
+		cd: "change the directory",
+		echo: "print something to console",
+		ls: "list the contents of this directory",
+		cat: "show the contents of a file",
+		mkdir: "makes a subdirectory (with directory name as the parameter)",
+		js: "executes javascript",
+		mkfile: "makes a file (with file name as the first parameter",
+		write: "write "
+	},
 	c: {
 		cd: e => {
-			if (e.length!==0&&e[0]!=="") {
-				let x = (n(e).startsWith("/")) ? e : t.dir + ((t.dir=="/")?"":"/") + n(e)
+			if (e.length !== 0 && e[0] !== "") {
+				let x = (n(e).startsWith("/")) ? e : t.dir + ((t.dir == "/") ? "" : "/") + n(e)
 				t.setdir(n(x));
 				t.t.set_prompt(t.dir + " $ ")
 			}
 		},
 		echo: e => t.t.echo(n(e)),
-		ls: ()=>{try{t.t.echo(Object.keys(t?.dirObj))}catch(e){t.t.error("err")}},
-		cat: e=>{if(t?.dirObj.hasOwnProperty(n(e))&&typeof t.dirObj[n(e)]!="object")t.t.echo(t.dirObj[n(e)])},
-		mkdir: e=>{t.dirObj[e]={};syncStore()},
-		"#":()=>{},
-		js:e=>eval(n(e)), //hmm
+		ls: () => { try { t.t.echo(Object.keys(t?.dirObj)) } catch (e) { t.t.error("error listing contents") } },
+		cat: e => { if (t.dirObj.hasOwnProperty(n(e)) && typeof t.dirObj[n(e)] != "object") t.t.echo(t.dirObj[n(e)]) },
+		mkdir: e => { t.dirObj[e] = {};
+			syncStore() },
+		"#": () => {},
+		js: e => console.log(eval(n(e))), //hmm
 	}
 }
 $(() => {
@@ -61,11 +78,16 @@ $(() => {
 			t.t = x
 			console.log = t.t.echo
 			console.error = t.t.error
-			if (localStorage.getItem("t") !== null) {
-					t.dirObj = JSON.parse(localStorage.t)
+			if (localStorage.getItem("t") != null) {
+				t.dirObj = JSON.parse(localStorage.t)
+				syncStore()
+			} 
+			if (Object.keys(store).length == 0) {
+				setTimeout(() => {
+					t.dirObj = def
 					syncStore()
-			}else{
-				store = def
+					console.log("welcome")
+				}, 500)
 			}
 		},
 		greetings: "",
