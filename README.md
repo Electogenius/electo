eb6b0d422bde76fe5f72a8e3092246a4cda3ac52
Electogenius <69585636+Electogenius@users.noreply.github.com>
2021-05-28 23:46:36 +0530

    yeet

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # electo
 My website I guess
+
+
+also yes there's a dark mode switcher "library" that sets the "data-dark" attribute of the body tag to "true" when in dark mode. It also adds a css link tag to average.css, which is just a bit of css I find useful when I'm too lazy. If you don't want average.css just add a data-customcss to the body tag
--- darkmodeswitch.js
+++ darkmodeswitch.js
@@ -1,13 +1,28 @@
 window.addEventListener("DOMContentLoaded",()=>{let f = document.createElement("link");
-if(document.querySelector("body[data-customcss]")!==document.body){f.rel="stylesheet";
+if(document.querySelector("body[data-customcss]")!==document.body){
+f.rel="stylesheet";
 f.href="https://electogenius.github.io/electo/average.css";}else{document.head.appendChild(document.createElement("style"))}
 document.head.appendChild(f);
 let d = document.createElement("span")
 d.innerText=""
-document.styleSheets[0].insertRule("body[data-dark] span.dmode-toggle::after{content:'☀️'}");document.styleSheets[0].insertRule("body:not([data-dark]) span.dmode-toggle::after{content:'🌙'}")
+document.styleSheets[0].insertRule("body[data-dark=\"true\"] span.dmode-toggle::after{content:'☀️'}");document.styleSheets[0].insertRule("body:not([data-dark=\"true\"]) span.dmode-toggle::after{content:'🌙'}")
 d.style="position:fixed;right:10px;bottom:10px;border-radius:30px;border:1px solid yellow;padding:10px;background:rgba(127,127,127,0.5);"
 d.onclick=()=>{
-	document.body.toggleAttribute("data-dark")
+	if (localStorage["dark-mode"] === "true") {
+		document.body.setAttribute("data-dark", "false")
+	} else {
+		document.body.setAttribute("data-dark", "true")
+	}
+	localStorage.setItem("dark-mode", document.body.getAttribute("data-dark"))
+	
 }
 d.classList.add("dmode-toggle")
-document.body.appendChild(d)})
+document.body.appendChild(d)
+if(localStorage.getItem("dark-mode")!==null){
+	if(localStorage["dark-mode"]==="true"){
+		document.body.setAttribute("data-dark", true)
+	}else{
+		document.body.setAttribute("data-dark", false)
+	}
+}
+})
