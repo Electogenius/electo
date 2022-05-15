81dd1dc6292679cb7b491b04ca787ef0c8a157a3
Ectogen‮ <69585636+Electogenius@users.noreply.github.com>
2022-05-15 12:00:57 +0530

    test

--- darkmodeswitch.js
+++ darkmodeswitch.js
@@ -1,28 +1,35 @@
-window.addEventListener("DOMContentLoaded",()=>{let f = document.createElement("link");
-if(document.querySelector("body[data-customcss]")!==document.body){
-f.rel="stylesheet";
-f.href="https://electogenius.github.io/electo/average.css";}else{document.head.appendChild(document.createElement("style"))}
-document.head.appendChild(f);
-let d = document.createElement("span")
-d.innerText=""
-document.styleSheets[0].insertRule("body[data-dark=\"true\"] span.dmode-toggle::after{content:'☀️'}");document.styleSheets[0].insertRule("body:not([data-dark=\"true\"]) span.dmode-toggle::after{content:'🌙'}")
-d.style="position:fixed;right:10px;bottom:10px;border-radius:30px;border:1px solid yellow;padding:10px;background:rgba(127,127,127,0.5);"
-d.onclick=()=>{
-	if (localStorage["dark-mode"] === "true") {
-		document.body.setAttribute("data-dark", "false")
-	} else {
-		document.body.setAttribute("data-dark", "true")
+window.addEventListener("DOMContentLoaded", () => {
+	function insertRule(rule) {
+		let sheet=document.createElement('style')
+		sheet.innerHTML = rule
+		document.head.appendChild(sheet)
 	}
-	localStorage.setItem("dark-mode", document.body.getAttribute("data-dark"))
-	
-}
-d.classList.add("dmode-toggle")
-document.body.appendChild(d)
-if(localStorage.getItem("dark-mode")!==null){
-	if(localStorage["dark-mode"]==="true"){
-		document.body.setAttribute("data-dark", true)
-	}else{
-		document.body.setAttribute("data-dark", false)
+	let f = document.createElement("link");
+	if (document.querySelector("body[data-customcss]") !== document.body) {
+		f.rel = "stylesheet";
+		f.href = "https://electogenius.github.io/electo/average.css";
+	} else { document.head.appendChild(document.createElement("style")) }
+	document.head.appendChild(f);
+	let d = document.createElement("span")
+	d.innerText = ""
+	insertRule("body[data-dark=\"true\"] span.dmode-toggle::after{content:'☀️'}"); insertRule("body:not([data-dark=\"true\"]) span.dmode-toggle::after{content:'🌙'}")
+	d.style = "position:fixed;right:10px;bottom:10px;border-radius:30px;border:1px solid yellow;padding:10px;background:rgba(127,127,127,0.5);"
+	d.onclick = () => {
+		if (localStorage["dark-mode"] === "true") {
+			document.body.setAttribute("data-dark", "false")
+		} else {
+			document.body.setAttribute("data-dark", "true")
+		}
+		localStorage.setItem("dark-mode", document.body.getAttribute("data-dark"))
+
 	}
-}
+	d.classList.add("dmode-toggle")
+	document.body.appendChild(d)
+	if (localStorage.getItem("dark-mode") !== null) {
+		if (localStorage["dark-mode"] === "true") {
+			document.body.setAttribute("data-dark", true)
+		} else {
+			document.body.setAttribute("data-dark", false)
+		}
+	}
 })
